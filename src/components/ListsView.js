66bb8a982a5b69471d25c68f846a634134ff32c2
Brent Colson <brentcolson@gmail.com>
2016-06-18 13:36:43 -0400

    Refactored ListsView to use ListView and TouchableHighlight

--- src/components/ListsView.js
+++ src/components/ListsView.js
@@ -2,7 +2,9 @@
 
 import React, { Component } from 'react';
 import {
-
+  View,
+  ListView,
+  TouchableHighlight
 } from 'react-native';
 import LineItem from './lineItem.js'
 
@@ -10,10 +12,40 @@ import styles from '../styles/styles.js'
 import tempData from '../data/tempData.js'
 
 
-export default class ListView extends Component {
-  render() {
-    return (
-      <LineItem item={ tempData[0] } />
-    );
+export default class ListsView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       dataSource: new ListView.DataSource({
+           rowHasChanged: (row1, row2) => row1 !== row2
+       })
+    };
   }
+  componentDidMount() {
+    var books = tempData;
+    this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(books)
+    });
+   }
+
+  renderItem(item) {
+    return (
+      <TouchableHighlight>
+        <View>
+          <LineItem item={ item } />
+        </View>
+      </TouchableHighlight>
+    );
+    }
+
+   render() {
+     return (
+       <ListView
+           dataSource={this.state.dataSource}
+           renderRow={this.renderItem.bind(this)}
+           style={styles.listView}
+         />
+     );
+   }
 }
